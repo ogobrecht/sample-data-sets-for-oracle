a02dd7173104927d8c747bd3f4c9ab121e3419e0
Ottmar Gobrecht <ogobrecht@users.noreply.github.com>
2020-04-07 20:49:38 +0200

    template for new plex export function table_to_forallinsert

--- dvd_rental_store/data/drs_film_test.sql
+++ dvd_rental_store/data/drs_film_test.sql
@@ -7,7 +7,35 @@ r     drs_film_tab;
 start pls_integer := dbms_utility.get_time;
 begin
 --
-
+r(1).film_id := 100001;
+r(1).title := 'ACADEMY DINOSAUR';
+r(1).description := 'A Epic Drama of a Feminist And a Mad Scientist who must Battle a Teacher in The Canadian Rockies';
+r(1).release_year := '2006';
+r(1).language_id := 1;
+r(1).original_language_id := null;
+r(1).rental_duration := 6;
+r(1).rental_rate := 0.99;
+r(1).length := 86;
+r(1).replacement_cost := 20.99;
+r(1).rating := 'PG';
+r(1).special_features := 'Deleted Scenes,Behind the Scenes';
+r(1).last_update := to_date('2020-04-06', 'yyyy-mm-dd');
+--
+r(2).film_id := 100002;
+r(2).title := 'ACADEMY DINOSAUR';
+r(2).description := 'A Epic Drama of a Feminist And a Mad Scientist who must Battle a Teacher in The Canadian Rockies';
+r(2).release_year := '2006';
+r(2).language_id := 1;
+r(2).original_language_id := null;
+r(2).rental_duration := 6;
+r(2).rental_rate := 0.99;
+r(2).length := 86;
+r(2).replacement_cost := 20.99;
+r(2).rating := 'PG';
+r(2).special_features := 'Deleted Scenes,Behind the Scenes';
+r(2).last_update := to_date('2020-04-06', 'yyyy-mm-dd');
+--
+-- ...
 --
 forall i in 1..r.count
   insert into drs_film (
