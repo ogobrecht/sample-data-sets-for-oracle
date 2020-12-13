a98719ee01877e808d76c848cb68578205059671
Ottmar Gobrecht <ogobrecht@users.noreply.github.com>
2020-12-13 15:34:21 +0100

    back to table_prefix instead of table_filter

--- _global_scripts/disable_foreign_key_constraints.sql
+++ _global_scripts/disable_foreign_key_constraints.sql
@@ -8,10 +8,10 @@ Options
 
 The first parameter of the script can contain two options:
 
-- table_filter:
-  - A like expression (escape char is '\')
-  - Example: `table_filter=CO\_%` will be expanded to `table_name like 'CO\_%' escape '\'`
-  - If omitted, it will default to '%' (matches all tables)
+- table_prefix:
+  - Given value will be uppercased, a right underscore be trimmed because we need to inject a backslash
+  - Example: `table_prefix=hr` will result in a filter expression `table_name like 'HR\_%' escape '\'`
+  - If omitted, it will default to NULL and match then all tables
 - dry_run:
   - `dry_run=true` will only report the intended work and do nothing
   - `dry_run=false` will do the intended work
@@ -20,8 +20,8 @@ The first parameter of the script can contain two options:
 Examples
 --------
 
-    @disable_foreign_key_constraints.sql "table_filter=%  dry_run=true"
-    @disable_foreign_key_constraints.sql "table_filter=CO\_%  dry_run=false"
+    @disable_foreign_key_constraints.sql "dry_run=true"
+    @disable_foreign_key_constraints.sql "table_prefix=co  dry_run=false"
 
 Meta
 ----
@@ -35,16 +35,19 @@ prompt DISABLE FOREIGN KEY CONSTRAINTS
 set define on serveroutput on verify off feedback off linesize 120
 
 declare
+  v_table_prefix varchar2(100);
   v_table_filter varchar2(100);
   v_dry_run      varchar2(100);
   v_count        pls_integer := 0;
 begin
-  v_table_filter := nvl(regexp_substr('&1','table_filter=([^ ]*)',1,1,'i',1), '%');
-  v_dry_run := nvl(lower(regexp_substr('&1','dry_run=(true|false)',1,1,'i',1)), 'true');
-  if v_table_filter = '%' then
+  v_table_prefix := rtrim(upper(regexp_substr('&1','table_prefix=([^ ]*)',1,1,'i',1)),'_');
+  v_dry_run      := nvl(lower(regexp_substr('&1','dry_run=(true|false)',1,1,'i',1)), 'true');
+  if v_table_prefix is null then
     dbms_output.put_line('- for all tables');
+    v_table_filter := '%';
   else
-    dbms_output.put_line('- for tables like ''' || v_table_filter || '''');
+    dbms_output.put_line('- for tables prefixed with ''' || v_table_prefix || '''');
+    v_table_filter := v_table_prefix || '\_%';
   end if;
   if v_dry_run = 'true' then
     dbms_output.put_line('- dry run entered');
